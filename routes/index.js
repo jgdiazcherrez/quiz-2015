45470f4dfbb7cb10287cf6b07267e6443f64319c
jgdiazcherrez <jonathantrece@gmail.com>
2015-08-15 20:32:42 +0200

    Modificamos controlador, para agregar control de errores en servidor a la hora de crear la pregunta

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[]});
 });
 
 /* Get Author*/
 router.get('/author', function(req, res) {
-  res.render('author');
+  res.render('author', {errors:[]});
 });
 
 
