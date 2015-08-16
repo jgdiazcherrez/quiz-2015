90c9bbbd3ea5ff5f4c2b8e8f561c816a7cced6ef
jgdiazcherrez <jonathantrece@gmail.com>
2015-08-16 12:27:19 +0200

    Agregamos force, para  generar db de nuevo

--- models/models.js
+++ models/models.js
@@ -29,7 +29,7 @@ exports.Quiz = Quiz;
 
 //Sincronizamos las definiciones que existen en el modelo '/models'
 //TODO: QUitar el force true cuando hagamos el desplegue final
-sequelize.sync().then(function(){
+sequelize.sync({force:true}).then(function(){
     Quiz.count().then(function(count){
             if(count === 0){
                 Quiz.create({
